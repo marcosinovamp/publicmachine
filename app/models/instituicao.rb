bd7fb48b1bc34e92b1c02de9ff8a571591b6fec4
Marcos Flores <marcoschines@gmail.com>
2022-09-12 20:51:46 -0300

    unidade

--- app/models/instituicao.rb
+++ app/models/instituicao.rb
@@ -1,21 +1,12 @@
 class Instituicao < ApplicationRecord
     has_many :uorgs
 
-    # def filhos
-    #     # @children = Instituicao.all.select{|i| i.pai == self.codigo}
-    #     @children = Uorg.all.select{|u| u.pai == self.codigo}
-    #     if @children.nil? == false || @children.empty? == false
-    #         @children.each do |c|
-    #             puts "#{c.nome} - #{c.sigla}"
-    #         end
-    #     end
-    # end
-    
-    # def pai
-    #     if self.pai.nil? == false || self.pai != 0
-    #         @pai = Uorg.find_by(codigo: self.pai)
-    #         puts "#{@pai.nome} - #{@pai.sigla}"
-    #     end
-    # end  
+    def children
+        Instituicao.all.select{|i| i.pai == self.codigo}
+    end
+
+    def howmuch
+        Instituicao.all.select{|i| i.pai == self.codigo}.size
+    end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'admin#home'
-  get 'unidade', to: 'admin#unidade'
+  get 'unidade/:id', to: 'admin#unidade'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
